0dacfb72418850985a3d793bed9092132db3580e
Emerson Valenzuela <52836406+EmersonValenzuela@users.noreply.github.com>
2024-07-27 05:50:05 -0500

    :fire: Settings Providers

--- app/Providers/SettingsServiceProvider.php
+++ app/Providers/SettingsServiceProvider.php
@@ -7,6 +7,7 @@ use App\Models\sunatCodeUbigeo;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\QueryException;
 
 class SettingsServiceProvider extends ServiceProvider
 {
@@ -37,30 +38,35 @@ class SettingsServiceProvider extends ServiceProvider
      */
     public static function updateSessionData()
     {
-        // Verificar si la tabla settings existe antes de intentar obtener datos
-        if (Schema::hasTable('settings')) {
-            $settings = Settings::first();
+        try {
+            // Verificar si la tabla settings existe antes de intentar obtener datos
+            if (Schema::hasTable('settings')) {
+                $settings = Settings::first();
 
-            // Agregar los datos de configuración a la sesión
-            if ($settings) {
-                Session::put('ruc', $settings->ruc);
-                Session::put('reason', $settings->reason);
-                Session::put('ecommerce', $settings->ecommerce);
-                Session::put('phone', $settings->phone);
-                Session::put('email', $settings->email);
-                Session::put('ubigeo', $settings->ubigeo);
-                Session::put('urbanization', $settings->urbanization);
-                Session::put('address', $settings->address);
+                // Agregar los datos de configuración a la sesión
+                if ($settings) {
+                    Session::put('ruc', $settings->ruc);
+                    Session::put('reason', $settings->reason);
+                    Session::put('ecommerce', $settings->ecommerce);
+                    Session::put('phone', $settings->phone);
+                    Session::put('email', $settings->email);
+                    Session::put('ubigeo', $settings->ubigeo);
+                    Session::put('urbanization', $settings->urbanization);
+                    Session::put('address', $settings->address);
 
-                // Verificar si la tabla ubigeo existe antes de intentar obtener datos
-                if (Schema::hasTable('sunat_codigoubigeo')) {
-                    $ubigeo = sunatCodeUbigeo::find($settings->ubigeo);
+                    // Verificar si la tabla ubigeo existe antes de intentar obtener datos
+                    if (Schema::hasTable('sunat_codigoubigeo')) {
+                        $ubigeo = sunatCodeUbigeo::find($settings->ubigeo);
 
-                    if ($ubigeo) {
-                        Session::put('ubigeo_data', $ubigeo);
+                        if ($ubigeo) {
+                            Session::put('ubigeo_data', $ubigeo);
+                        }
                     }
                 }
             }
+        } catch (QueryException $e) {
+            // Manejar el error de consulta aquí (opcionalmente registrar el error)
+            // Puedes usar Log::error($e) para registrar el error en un archivo de log
         }
     }
 }
